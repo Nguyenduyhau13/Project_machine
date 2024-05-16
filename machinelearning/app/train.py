5aa199f8f7c334f8a22382cdfe1f9cea450b825a
Phó Tiến Anh <pvtatienanh@gmail.com>
2024-05-16 22:56:23 +0700

    ui

---
+++ machinelearning/app/train.py
@@ -0,0 +1,4 @@
+def model():
+    pass
+def get_prediction(model, input_dict):
+    return "Negative"
--- machinelearning/app/views.py
+++ machinelearning/app/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from app.train import model, get_prediction
+
+
 def index(request):
-    return render(request, 'app/index.html')
+    context = {
+        'status': 'Input test results'
+    }
+    return render(request, 'app/index.html', context)
+
+
+def predict(request):
+    context = {
+        'status': 'Input test results'
+    }
+    if request.method == "POST":
+        prediction = get_prediction(model, request.POST)
+        context = {
+            'status': f"Prediction = {prediction}"
+        }
+        features = ["diagnosis","radius_mean","texture_mean","perimeter_mean","area_mean","smoothness_mean",
+                    "compactness_mean","concavity_mean","concave_points_mean","symmetry_mean",
+                    "fractal_dimension_mean","radius_se","texture_se","perimeter_se","area_se","smoothness_se",
+                    "compactness_se","concavity_se","concave_points_se","symmetry_se","fractal_dimension_se",
+                    "radius_worst","texture_worst","perimeter_worst","area_worst","smoothness_worst",
+                    "compactness_worst","concavity_worst","concave_points_worst","symmetry_worst","fractal_dimension_worst"]
+        for key in features:
+            if key in request.POST:
+                context[key] = request.POST[key]
+    return render(request, 'app/index.html', context)
