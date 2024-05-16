3d479224622d189c233309be5f2f8e7467612414
Duy Hau <duyhautd13@gmail.com>
2024-05-17 00:23:42 +0700

    changes

--- machinelearning/app/train.py
+++ machinelearning/app/train.py
@@ -1,4 +1,20 @@
+import joblib
+import numpy as np
+
 def model():
-    pass
+    # Load the trained model
+    loaded_model = joblib.load('D:\MC\Project_machine\knn.pkl')
+    return loaded_model
+
 def get_prediction(model, input_dict):
-    return "Negative"
+    model= model()
+    numeric_input = {k: float(v) for k, v in input_dict.items() if isinstance(v, (int, float)) or v.isnumeric()}
+
+    input_data = np.array(list(numeric_input.values())).reshape(1, -1)
+
+    prediction = model.predict(input_data)
+
+    labels = {0: "lành tính", 1: "ác tính"}
+    prediction_label = labels[prediction[0]]
+
+    return prediction_label
